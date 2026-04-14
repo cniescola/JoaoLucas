0353e320abf591d9c8dcbe655f76d345140ae099
vitor brenno <cniescola85@gmail.com>
2026-04-14 15:53:01 -0300

    projeto final

---
+++ PROJETO FINAL/conexão.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "localhost:3306";
+    $usuario = "root";
+    $senha = "cniaraguari85";
+    $banco = "produtos";
+
+    $con = new mysqli($host,$usuario,$senha,$banco);
+
+    
+
+?>
