a44212176f09b8e4c5b7fadfe2cad5032c9f0395
vitor brenno <cniescola85@gmail.com>
2026-05-07 14:31:45 -0300

    parte do cadastro pagina montagem produto

--- PROJETO FINAL/cadastro.php
+++ PROJETO FINAL/cadastro.php
@@ -8,11 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+     
 </head>
 
 <body>
-
-
     <header>
         <div class="alfa">
             <a href="index.php" class="texto btn btn-transparent-dark btn-sm">
--- PROJETO FINAL/paginas/AdicionarProduto.php
+++ PROJETO FINAL/paginas/AdicionarProduto.php
@@ -1,3 +1,76 @@
-<section>
-    
+<section class="adicionar container w-100 h-100 p-10 mt-4">
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card mb-4">
+                <div class="card-body">
+
+                    <h3 class="h6 mb-4">Cadastro do produto</h3>
+
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="mb-3">
+                                <label class="form-label">Nome Do produto</label>
+                                <input type="text" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <label class="form-label">Codigo do Produto</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <div class="mb-3">
+                                <label for="textarea1" class="form-label">Digite a descrição do produto</label>
+                                <textarea id="textarea1" class="form-control" rows="7"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="row d-flex flex-column justify-content-between h-100">
+                                <div class="mb-3">
+                                    <label class="form-label">Preço Custo R$</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="number" class="form-control d-flex w-50" placeholder="% Lucro">
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Preço R$</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 d-flex flex-column justify-content-between">
+            <div class="row">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <label class="form-label">Digite o Link</label>
+                        <input type="text" class="form-control mb-2">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <label class="form-label">Numero de parcelas</label>
+                        <input type="number" class="form-control mb-2" max="24">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <label class="form-label">Porentagem de Desconto</label>
+                        <input type="number" class="form-control mb-2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
